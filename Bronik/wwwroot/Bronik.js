29a503a317156fa61a48b9ece00ee7ba2eb50bea
Andrew <kaibazakov@yandex.ru>
2022-06-14 11:46:08 +0700

    Поправил формы

--- Bronik/wwwroot/Bronik.js
+++ Bronik/wwwroot/Bronik.js
@@ -5,11 +5,11 @@ let unlock = true;
 
 const timeout = 800;
 
-if (popupLinks.length > 0){
-    for (let index = 0; index < popupLinks.length; index++){
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
         const popup_link = popupLinks[index];
-        popup_link.addEventListener("click", function (e){
-            const popupName = popup_link.getAttribute('href').replace('#','');
+        popup_link.addEventListener("click", function (e) {
+            const popupName = popup_link.getAttribute('href').replace('#', '');
             const curentPopup = document.getElementById(popupName);
             popupOpen(curentPopup);
             e.preventDefault();
@@ -17,30 +17,30 @@ if (popupLinks.length > 0){
     }
 }
 const popupCloseIcon = document.querySelectorAll('.close-popup');
-if (popupCloseIcon.length > 0){
-    for (let index = 0; index < popupCloseIcon.length; index++){
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
         const el = popupCloseIcon[index];
-        el.addEventListener('click', function (e){
+        el.addEventListener('click', function (e) {
             popupClose(el.closest('.popup'));
             e.preventDefault();
         });
     }
 }
 
-function popupOpen(curentPopup){
-    if (curentPopup && unlock){
+function popupOpen(curentPopup) {
+    if (curentPopup && unlock) {
         const popupActive = document.querySelector('.popup.open');
     }
     curentPopup.classList.add('open');
-    curentPopup.addEventListener("click", function (e){
-        if (!e.target.closest('.infowindow_content')){
+    curentPopup.addEventListener("click", function (e) {
+        if (!e.target.closest('.infowindow_content')) {
             popupClose(e.target.closest('.popup'));
         }
     });
 }
 
-function popupClose(popupActive){
-    if (unlock){
+function popupClose(popupActive) {
+    if (unlock) {
         popupActive.classList.remove('open')
     }
 }
